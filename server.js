2e6ffcb2cb883677585a637582095471864767f7
saptyam1234-commits <saptyam1234@gmail.com>
2026-02-14 02:22:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend Running Successfully 🚀");
+});
+
+const chatRoutes = require("./routes/chat.routes");
+app.use("/api/chat", chatRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
