9f3f97bdc5a33df6beaa2e5e4dba49815fab19d8
saptyam1234-commits <saptyam1234@gmail.com>
2026-02-15 17:23:06 +0530

    Update prompt.service.js

--- prompt.service.js
+++ prompt.service.js
@@ -1,1 +1,5 @@
+function generatePrompt(userMessage, scanData = '') {
+    return `Use the following website info: ${scanData}\nRespond to user query: ${userMessage}`;
+}
 
+module.exports = { generatePrompt };
