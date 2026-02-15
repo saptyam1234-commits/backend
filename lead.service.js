acc58dc39ad1a3dc80c670a93369bbfcb25e26b4
saptyam1234-commits <saptyam1234@gmail.com>
2026-02-15 17:29:36 +0530

    Update lead.service.js

--- lead.service.js
+++ lead.service.js
@@ -1,1 +1,19 @@
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
 
+const serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+};
+
+initializeApp({ credential: cert(serviceAccount) });
+const db = getFirestore();
+
+async function saveLead(data) {
+    const ref = db.collection('leads').doc();
+    await ref.set(data);
+    return { id: ref.id, ...data };
+}
+
+module.exports = { saveLead };
