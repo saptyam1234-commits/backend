967a605d762972a2d88773f64ae23352d5e7e315
saptyam1234-commits <saptyam1234@gmail.com>
2026-02-15 17:27:05 +0530

    Update scan.service.js

--- scan.service.js
+++ scan.service.js
@@ -1,1 +1,17 @@
+const fetch = require('node-fetch');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 
+async function scanWebsite(url) {
+    const res = await fetch(url);
+    const html = await res.text();
+    const dom = new JSDOM(html);
+    
+    const title = dom.window.document.querySelector('title')?.textContent || '';
+    const metaDesc = dom.window.document.querySelector('meta[name="description"]')?.getAttribute('content') || '';
+    const h1 = dom.window.document.querySelector('h1')?.textContent || '';
+
+    return { title, metaDesc, h1, url };
+}
+
+module.exports = { scanWebsite };
