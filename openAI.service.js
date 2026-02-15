56a4680fd44b13b4007afb49e7341c37cc71f9c4
saptyam1234-commits <saptyam1234@gmail.com>
2026-02-15 11:41:25 -0500

    Update openAI.service.js

--- openAI.service.js
+++ openAI.service.js
@@ -1,14 +1,7 @@
-const { Configuration, OpenAIApi } = require('openai');
+const OpenAI = require("openai");
 
-const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
-async function getChatResponse(message) {
-    const response = await openai.chat.completions.create({
-        model: 'gpt-4o-mini',
-        messages: [{ role: 'user', content: message }]
-    });
-    return response.data.choices[0].message.content;
-}
-
-module.exports = { getChatResponse };
+module.exports = openai;
