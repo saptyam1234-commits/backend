2cb36f7852af3b8d75be28274f13dcec2b3d1b66
saptyam1234-commits <saptyam1234@gmail.com>
2026-02-15 17:17:09 +0530

    Update widget.routes.js

--- widget.routes.js
+++ widget.routes.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { getWidgetSnippet } = require('./widget.service');
 
+router.get('/', (req, res) => {
+    const snippet = getWidgetSnippet();
+    res.send(snippet);
+});
+
+module.exports = router;
