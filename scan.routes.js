8f16cd6ee6890479867123981b834e6161c5c7aa
saptyam1234-commits <saptyam1234@gmail.com>
2026-02-15 17:16:17 +0530

    Update scan.routes.js

--- scan.routes.js
+++ scan.routes.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const { scanWebsite } = require('./scan.service');
 
+router.post('/', async (req, res) => {
+    try {
+        const { url } = req.body;
+        const data = await scanWebsite(url);
+        res.json({ scannedData: data });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+module.exports = router;
