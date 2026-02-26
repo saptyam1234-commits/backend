1fc70134b560962eac739d05eae67b3ad2d3e629
saptyam1234-commits <saptyam1234@gmail.com>
2026-02-27 04:05:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
 
 const chatRoutes = require('./chatRoutes');
 const leadRoutes = require('./leadRoutes');
@@ -13,7 +15,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Test route (important for debug)
+// Test route
 app.get('/', (req, res) => {
   res.send("API Running 🚀");
 });
@@ -25,4 +27,7 @@ app.use('/api/scan', scanRoutes);
 app.use('/api/widget', widgetRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
