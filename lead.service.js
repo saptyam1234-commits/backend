26af7d81e4bd767c8dc67ab2af4c9dd12eec2f1e
saptyam1234-commits <saptyam1234@gmail.com>
2026-02-27 20:10:25 +0530

    Update lead.service.js

--- lead.service.js
+++ lead.service.js
@@ -1,13 +1,20 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
+if (!process.env.FIREBASE_PRIVATE_KEY) {
+    throw new Error("FIREBASE_PRIVATE_KEY is missing in environment variables");
+}
+
 const serviceAccount = {
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
 };
 
-initializeApp({ credential: cert(serviceAccount) });
+initializeApp({
+    credential: cert(serviceAccount)
+});
+
 const db = getFirestore();
 
 async function saveLead(data) {
