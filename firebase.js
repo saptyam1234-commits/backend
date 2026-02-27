943b51e195838fc5a6147b29c91b1d51a622d220
saptyam1234-commits <saptyam1234@gmail.com>
2026-02-27 19:45:12 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,22 @@
-const admin = require("firebase-admin");
+  const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
 
-  const serviceAccount = JSON.parse(
-    process.env.FIREBASE_SERVICE_ACCOUNT
-  );
+  if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+    throw new Error("FIREBASE_SERVICE_ACCOUNT is missing");
+  }
+
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+  if (!serviceAccount.private_key) {
+    throw new Error("Private key is missing in service account");
+  }
 
   admin.initializeApp({
     credential: admin.credential.cert({
-      ...serviceAccount,
-      private_key: serviceAccount.private_key.replace(/\\n/g, '\n'),
+      projectId: serviceAccount.project_id,
+      clientEmail: serviceAccount.client_email,
+      privateKey: serviceAccount.private_key.replace(/\\n/g, '\n'),
     }),
     databaseURL: "https://konrk-88f13.firebaseio.com"
   });
@@ -17,4 +24,3 @@ if (!admin.apps.length) {
 }
 
 const db = admin.firestore();
-module.exports = db;  
