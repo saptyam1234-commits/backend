26db3c48a884c1f68a6c41e4dc2a5748e131121e
saptyam1234-commits <saptyam1234@gmail.com>
2026-02-27 14:51:03 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,19 @@
 const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
+
+  const serviceAccount = JSON.parse(
+    process.env.FIREBASE_SERVICE_ACCOUNT
+  );
+
   admin.initializeApp({
-    credential: admin.credential.applicationDefault()
+    credential: admin.credential.cert({
+      ...serviceAccount,
+      private_key: serviceAccount.private_key.replace(/\\n/g, '\n'),
+    }),
+    databaseURL: "https://konrk-88f13.firebaseio.com"
   });
+
 }
 
 const db = admin.firestore();
-
-module.exports = db;
